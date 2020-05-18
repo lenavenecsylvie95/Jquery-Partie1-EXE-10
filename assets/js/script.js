71725e999c18b9e7e8f06229302ef9e7ab1874de
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-19 00:00:39 +0200

    exercice10

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//On est prêt à interpréter du jquery
+$(function(){
+
+    $('#green').mouseenter(function(){
+        $('#text').css('color','green');
+    });
+
+  $('#green').mouseleave(function(){
+        $('#text').css('color','black');
+    });
+
+    $('#red').mouseenter(function(){
+        $('#text').css('color','red');
+    });
+
+  $('#red').mouseleave(function(){
+        $('#text').css('color','black');
+    });
+
+    $('#blue').mouseenter(function(){
+        $('#text').css('color','blue');
+    });
+
+    $('#blue').mouseleave(function(){
+        $('#text').css('color','black');
+    });
+
+});
